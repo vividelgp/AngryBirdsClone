68ac36fa4ac129dbf9c77a6e9ae4595a8979dd6a
vividelgp <vivianadelgp@hotmail.com>
2025-03-23 16:05:34 -0500

    feat: Add slingshot mechanics with mouse interaction
    - Clamped the slingshot's drag distance for balanced gameplay.
    - Restricted mouse click detection to the slingshot's vicinity for intuitive controls.

---
+++ Assets/Scripts/SlingShotArea.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class SlingShotArea : MonoBehaviour
+{
+    [SerializeField] private LayerMask _slingshotAreaMask;
+
+       public bool IsWithinSlingshotArea()
+       {
+          Vector2 worldPosition = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+
+          if (Physics2D.OverlapPoint(worldPosition, _slingshotAreaMask))
+          {
+            return true;
+          }
+          else
+          {
+              return false;
+          }
+        }
+}
--- Assets/Scripts/SlingShotHandler.cs
+++ Assets/Scripts/SlingShotHandler.cs
@@ -6,19 +6,71 @@ using UnityEngine.InputSystem;
 
 public class SlingShotHandelet : MonoBehaviour
 {
-    private LineRenderer _leftlineRenderer;
-    private LineRenderer _rightlineRenderer;
+    [Header("Line Renderers")]
+    [SerializeField] private LineRenderer _leftLineRenderer;
+    [SerializeField] private LineRenderer _rightLineRenderer;
+
+    [Header("Transform References")]
+    [SerializeField] private Transform _leftStartPosition;
+    [SerializeField] private Transform _rightStartPosition;
+    [SerializeField] private Transform _centerPosition;
+    [SerializeField] private Transform _idlePosition;
+
+    [Header("Slingshot Stats")]
+    [SerializeField] private float _maxDistance = 3.5f;
+
+    [Header("Scripts")]
+    [SerializeField] private SlingShotArea _slingShotArea;
+
+    private Vector2 _slingShotLinesPosition;
+
+    private bool _clickedWithinArea;
+
+    private void Awake()
+    {
+        _leftLineRenderer.enabled = false;
+        _rightLineRenderer.enabled = false;
+    }
 
     private void Update()
     {
-        if (Mouse.current.leftButton.wasPressedThisFrame)
+        if (Mouse.current.leftButton.wasPressedThisFrame && _slingShotArea.IsWithinSlingshotArea())
+        {
+            _clickedWithinArea = true;
+        }
+
+        if (Mouse.current.leftButton.isPressed && _clickedWithinArea)
         {
             DrawSlingShot();
+        }
+
+        if (Mouse.current.leftButton.wasReleasedThisFrame) 
+        {
+            _clickedWithinArea = false;
         }
     }
 
     private void DrawSlingShot()
     {
+        if (!_leftLineRenderer.enabled && !_rightLineRenderer.enabled)
+        {
+            _leftLineRenderer.enabled = true;
+            _rightLineRenderer.enabled = true;
+        }
+
         Vector3 touchPosition = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+
+        _slingShotLinesPosition = _centerPosition.position + Vector3.ClampMagnitude(touchPosition - _centerPosition.position, _maxDistance);
+
+        SetLines(_slingShotLinesPosition);
+    }
+
+    private void SetLines(Vector2 position)
+    {
+        _leftLineRenderer.SetPosition(0, position);
+        _leftLineRenderer.SetPosition(1, _leftStartPosition.position);
+
+        _rightLineRenderer.SetPosition(0, position);
+        _rightLineRenderer.SetPosition(1, _rightStartPosition.position);
     }
 }
